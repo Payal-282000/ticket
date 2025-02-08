c19234f8f58f8b2d788f0e290fd00d2f4a849f98
Payal-282000 <pbad2809@gmail.com>
2025-02-08 15:08:42 +0530

    CodeRefactor

---
+++ booking-ms/booking-ms/src/test/java/booking_ms/booking_ms/BookingMsApplicationTests.java
@@ -0,0 +1,13 @@
+package booking_ms.booking_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
